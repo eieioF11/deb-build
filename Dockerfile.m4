daed547d99a4388a81fd69da5482334fd42afce1
f11 <eieioeiji0501@gmail.com>
2024-10-18 20:46:04 +0900

    [add]first commit

---
+++ Dockerfile.m4
@@ -0,0 +1,16 @@
+FROM ros:DISTRO
+
+RUN apt update && apt install -y \
+  curl \
+  python3-bloom \
+  python3-rosdep \
+  fakeroot \
+  debhelper \
+  dh-python \
+  && apt clean && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /workspace
+
+COPY entrypoint.sh /
+
+CMD [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+# for libk4a
+echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | debconf-set-selections
+echo 'libk4a1.4 libk4a1.4/accept-eula boolean true' | debconf-set-selections
+
+APT_LIST=/etc/apt/sources.list.d/actions-local.list
+ROSDEP_LIST=/etc/ros/rosdep/sources.list.d/99-actions-local.list
+
+mkdir /repository
+
+for pkg_path in $(colcon list -tp); do
+  (
+    cd $pkg_path
+
+    apt update && rosdep update
+    rosdep install -yi --from-paths .
+    bloom-generate rosdebian
+    fakeroot debian/rules binary
+
+    apt_name=$(cat debian/control | awk '$1 == "Package:" { print $2 }')
+    rosdep_name=${apt_name#ros-*-}
+    rosdep_name=${rosdep_name//-/_}
+    deb_path=$(find $(cd .. && pwd) -name $apt_name'*.deb')
+
+    cp $deb_path /archives/
+
+    cd /repository
+
+    ln -s $deb_path .
+    dpkg-scanpackages . > Packages
+    if [ ! -e $APT_LIST ]; then
+      echo "deb [trusted=yes] file:///repository ./" > $APT_LIST
+    fi
+
+    echo -e "$rosdep_name:\n  ubuntu: [$apt_name]" >> rosdep.yaml
+    if [ ! -e $ROSDEP_LIST ]; then
+      echo "yaml file:///repository/rosdep.yaml" > $ROSDEP_LIST
+    fi
+  )
+done
